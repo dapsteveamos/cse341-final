5143f31ec921adf59e485a2f6b5a3da792e1649c
Micaela Tapia <mclmrls29@gmail.com>
2025-06-06 21:48:34 -0400

    student endpoints

--- routes/student.js
+++ routes/student.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 const studentController = require('../controllers/student.js');
 
-// router.get('/', );
-// router.get('/:id', );
-// router.post('/', )
-// router.put('/:id', );
-// router.delete('/:id', );
+router.get('/',  studentController.getAll);
+router.get('/:id', studentController.getSingle);
+router.post('/', studentController.createStudent);
+router.put('/:id', studentController.updateStudent);
+router.delete('/:id', studentController.deleteStudent);
 
 module.exports = router;
