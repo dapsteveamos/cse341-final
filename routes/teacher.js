79505108780520d4158212db300d689ef47a427c
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2025-06-13 21:25:41 -0300

    teachers routes and controllers added

--- routes/teacher.js
+++ routes/teacher.js
@@ -5,6 +5,32 @@ const router = express.Router();
 
 const teacherController = require('../controllers/teacher.js');
 
+const teacherValidationRules = {
+    name: 'required|string|min:3',
+    email: 'required|email',
+    subject: 'required|string|min:2'
+};
+
+router.get('/', isAuthenticated, teacherController.getAll);
+
+router.get('/:id', isAuthenticated, teacherController.getSingle);
+
+router.post(
+    '/',
+    isAuthenticated,
+    validation.validate(teacherValidationRules),
+    teacherController.createTeacher
+);
+
+router.put(
+    '/:id',
+    isAuthenticated,
+    validation.validate(teacherValidationRules),
+    teacherController.updateTeacher
+);
+
+router.delete('/:id', isAuthenticated, teacherController.deleteTeacher);
+
 // router.get('/', );
 // router.get('/:id', );
 // router.post('/', )
